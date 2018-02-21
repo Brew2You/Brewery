1a491e43ce016184e8e1363ac9d08c94ef31e73b
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 13:14:57 -0500

    generated component store.

--- Interface/src/app/store/store.module.ts
+++ Interface/src/app/store/store.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreComponent } from './store.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [StoreComponent]
 })
 export class StoreModule { }
