42c9b4db063855f85bfb4216f6d9c7802957b59e
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 10:38:13 -0500

    created db context for store.

---
+++ Store/Brewery.Store.Context/BrewDBC.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewery.Store.Context
+{
+    public class BrewDBC : DbContext
+    {
+    }
+}
