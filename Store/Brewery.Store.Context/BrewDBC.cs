d641866b74b851b5a276105a7da6068f252a4857
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 11:30:55 -0500

    implemented db context..

--- Store/Brewery.Store.Context/BrewDBC.cs
+++ Store/Brewery.Store.Context/BrewDBC.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Brewery.Store.Library.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,9 @@ namespace Brewery.Store.Context
 {
     public class BrewDBC : DbContext
     {
+        public BrewDBC(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Beer> Beer { get; set; }
     }
 }
