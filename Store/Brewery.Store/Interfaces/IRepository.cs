6615c415f2fef6b293645ea1e03520d93df4d907
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 09:51:23 -0500

    created repository interface.

---
+++ Store/Brewery.Store/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewery.Store.Library.Interfaces
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T GetById();
+        void Create();
+        void Update();
+        void Delete();
+    }
+}
