81a678d74e7558485b8661dd66f2ec175b12e008
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 10:32:07 -0500

    created beer model.

---
+++ Store/Brewery.Store/Models/Beer.cs
@@ -0,0 +1,12 @@
+﻿using Brewery.Store.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewery.Store.Library.Models
+{
+    public class Beer : IBeer
+    {
+        public string ModelId { get; set; }
+    }
+}
