4cc85984676c3939c7ef39b053f24cd9ea9b108a
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 13:19:17 -0500

    added store service to providers for store module.

--- Interface/src/app/store/store.module.ts
+++ Interface/src/app/store/store.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
+import { StoreService } from './store.service';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  providers: [
+    StoreService
   ],
   declarations: [StoreComponent]
 })
