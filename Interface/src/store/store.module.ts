ecfdfd42643c6a4695bcde027e658e9d00a2f74d
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 14:28:05 -0500

    added exports for store module.

--- Interface/src/store/store.module.ts
+++ Interface/src/store/store.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
 import { StoreService } from './store.service';
+import { AppRoutingModule } from '../app/app-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    StoreComponent
   ],
   providers: [
     StoreService
   ],
-  declarations: [StoreComponent]
+  declarations: [
+    StoreComponent
+  ]
 })
 export class StoreModule { }
