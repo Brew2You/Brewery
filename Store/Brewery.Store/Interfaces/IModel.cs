acc7424a89c2239e55d36e6198252e109b5b1443
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 10:27:31 -0500

    created model interface.

---
+++ Store/Brewery.Store/Interfaces/IModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brewery.Store.Library.Interfaces
+{
+    public interface IModel
+    {
+        string ModelId { get; }
+    }
+}
