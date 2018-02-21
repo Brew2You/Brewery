2eca01a8aade81e3577361f498bee2d5aa2fbb53
tyler hendrick <hendricktyler5@gmail.com>
2018-02-21 15:35:48 -0500

    added get Inventory function.

--- Interface/src/store/store.service.ts
+++ Interface/src/store/store.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class StoreService {
 
-  constructor() { }
+  private StoreURL = '';
+  private _port = 'Change this to start';
 
+  constructor(
+    @Inject(Document) private _doc: any,
+    private _http: HttpClient
+  ) { 
+    this.StoreURL = _doc.location.protocol + '//' + _doc.location.hostname + this._port + '/api/store';
+  }
+
+  getInventory(){
+    return this._http.get(this.StoreURL).toPromise();
+  }
 }
