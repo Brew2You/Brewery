855a75b476a632fe042e96c1fa0e71e6bb4c6483
tyler hendrick <hendricktyler5@gmail.com>
2018-02-27 10:21:50 -0500

    added dashboard routing.

---
+++ Interface/src/app/dashboard-routing/dashboard-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent, children: [
+    {
+      path: '', redirectTo: 'welcome', pathMatch: 'full'
+    },
+    {
+      path: 'welcome', loadChildren: '/app/welcome/welcome.module#WelcomeModule'
+    }
+  ]}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class DashboardRoutingModule { }
