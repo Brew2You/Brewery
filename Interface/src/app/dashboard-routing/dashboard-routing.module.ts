dd72f5360da498659c11d67f1e9f800234ba0290
tyler hendrick <hendricktyler5@gmail.com>
2018-02-27 10:36:35 -0500

    finished basic routing.

--- Interface/src/app/dashboard-routing/dashboard-routing.module.ts
+++ Interface/src/app/dashboard-routing/dashboard-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent, children: [
+  {path: '', component: DashboardComponent, children: [
     {
       path: '', redirectTo: 'welcome', pathMatch: 'full'
     },
     {
-      path: 'welcome', loadChildren: '/app/welcome/welcome.module#WelcomeModule'
+      path: 'welcome', loadChildren: '../welcome/welcome.module#WelcomeModule'
     }
   ]}
 ];
