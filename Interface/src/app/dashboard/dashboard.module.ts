32d4a9467568e185750685b3b90ca53faf476891
tyler hendrick <hendricktyler5@gmail.com>
2018-02-27 10:36:47 -0500

    added import for side nav.

--- Interface/src/app/dashboard/dashboard.module.ts
+++ Interface/src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from '../dashboard-routing/dashboard-routing.module';
+import { MatSidenavModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatSidenavModule
   ],
   declarations: [DashboardComponent]
 })
